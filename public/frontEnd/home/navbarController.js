1fd2a1ad849590ed788294818620b73d64626b17
Wayfearer <lico1@net.hr>
2015-11-18 14:56:07 +0100

    navbar rework

--- public/frontEnd/home/navbarController.js
+++ public/frontEnd/home/navbarController.js
@@ -5,18 +5,10 @@
 		.module('FixYourCityApp')
 		.controller('navbarController', navbarController);
 	
-	/*
-	*
-	*	TESTING
-	*
-	*/
-	
-	function navbarController(){
+	function navbarController($sce){
 		var vm = this;
-		/*
-		vm.activeAbout=activeAbout;
-		vm.hoverAbout=hoverAbout;
-		*/
+		vm.renderTabs=renderTabs;
+		
 		
 		vm.tabs=[
 			//BEGIN TABS
@@ -31,13 +23,13 @@
 					url: 'https://www.google.com/?q=angular',
 					target: '_blank',  
 					icoSrc: 'https://www.google.hr/images/branding/product/ico/googleg_lodp.ico',
-					innerDesc: 'Google (q=angular)',															/*<sup> Angular</sup>', */   //html render, $sce ? angular-sanitize ?
+					innerDesc: 'Google<sup> Angular</sup>',
 				},
 				{
 					url: 'https://github.com/matija077/FixYourCity',
 					target: '_blank',   
 					icoSrc: 'https://github.com/fluidicon.png',
-					innerDesc: 'GitHub (FixYourCity)', 															/*'<sup> FixYourCity</sup>',*/
+					innerDesc: 'GitHub<sup> FixYourCity</sup>', 															
 				},
 				{
 					url: 'https://github.com/johnpapa/angular-styleguide#table-of-contents',
@@ -49,7 +41,7 @@
 					url: 'http://laravel.com/docs/5.1/',
 					target: '_blank',  
 					icoSrc: 'http://laravel.com/favicon.png',
-					innerDesc: 'Laravel (Doc)',																	/*<sup> Documentation</sup>',*/
+					innerDesc: 'Laravel<sup> Documentation</sup>',																
 				},
 				{
 					url: 'http://getbootstrap.com/components/',
@@ -60,7 +52,7 @@
 				{
 					url: 'http://localhost/FixYourCity/public/#/',
 					target: '',  
-					icoSrc: 'https://upload.wikimedia.org/wikipedia/commons/6/63/Number_sign.svg',
+					icoSrc: '',
 					innerDesc: 'About',
 				},
 				
@@ -90,20 +82,12 @@
 			*/
 		
 		
-		
-		
-		
-		//activeAbout();
-		
-		/*
-		function hoverAbout(){
-			vm.InnerAbout="Fake link";
+		function renderTabs(tab){     //called for each tab in navbar from html using ng-repeat
+			var retval=''; 
+			retval='<a href="'+tab.url+'" target="'+tab.target+'"><img width="16px" height="16px" src="'+tab.icoSrc+'"/> '+tab.innerDesc+'</a>';
+			
+			return $sce.trustAsHtml(retval);    // 'accepts' it as a html code
 		}
-		
-		function activeAbout(){
-			vm.InnerAbout="About";
-		}
-		*/
 		
 	}
 

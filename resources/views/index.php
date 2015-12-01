b7711aedcd27223fae6f2be5973337625b78f08f
stagg250 <stagg250@gmail.com>
2015-12-01 18:57:13 +0100

    salkotest1

--- resources/views/index.php
+++ resources/views/index.php
@@ -15,6 +15,9 @@
 			<div ng-include = "'frontEnd/home/navbar.htm'"></div>
 		</div>
 		<div ng-view></div>
+		<div class="footernavbar" >
+			<div ng-include = "'frontEnd/home/footer.htm'"></div>
+		</div>
 	</div>	
 	
 	<noscript>
@@ -29,6 +32,13 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-cookies.js"></script>
 	<script src="http://cdnjs.cloudflare.com/ajax/libs/restangular/1.3.1/restangular.js"></script>
 	
+	  <!-- Angular Material Javascript now available via Google CDN; version 0.11.2 used here -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/angular_material/0.11.2/angular-material.min.js"></script> -->
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-aria.min.js"></script>
+	 <!-- Angular Material CSS now available via Google CDN; version 0.11.2 used here -->
+  <!--  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/0.11.2/angular-material.min.css"> -->
+	
 	<script src="frontEnd/module.js"></script>
 	
 	<script src="frontEnd/home/HomeController.js"></script>
@@ -38,6 +48,9 @@
 	<script src="frontEnd/dataservice.js"></script>
 	
 	<script src="frontEnd/home/navbarController.js"></script>
+	
+	<script src="frontEnd/home/AppCtrl.js"></script>
+	
 	
 </body>
 

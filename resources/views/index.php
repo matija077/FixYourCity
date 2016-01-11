8b5d108328a1d61211785bbee8e8ab9edbc58557
Wayfearer <lico1@net.hr>
2016-01-11 13:23:24 +0100

    using only minified upload libraries, removed the rest

--- resources/views/index.php
+++ resources/views/index.php
@@ -62,8 +62,8 @@
 	<script src="frontend/home/suggestCategoryController.js"></script>
 	
 	<!-- <script src="angular(.min).js"></script> -->
-	<script src="frontend/ng-file-upload-bower-11.0.2/ng-file-upload-shim.min.js"></script> <!-- for no html5 browsers support -->
-	<script src="frontend/ng-file-upload-bower-11.0.2/ng-file-upload.min.js"></script>
+	<script src="frontend/ng-file-upload/ng-file-upload-shim.min.js"></script> <!-- for no html5 browsers support -->
+	<script src="frontend/ng-file-upload/ng-file-upload.min.js"></script>
 	
 </body>
 

d52cc7b1c658d277c6ece1c543f7e54598a54f5a
salkomaci <stagg250@gmail.com>
2016-01-17 20:46:07 +0100

    routes.php fix

---
+++ app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', 'HomeController@index');
+
+Route::group(array('prefix' => 'api'), function() 
+{
+	Route::get('/cities/{id}', 'ApiController@getCity');
+	Route::get('/cities', 'ApiController@getCities');
+	Route::get('/categories', 'ApiController@getCategories');
+	Route::get('/categories/{id}', 'ApiController@getCategory');
+	Route::post('/insertcity', 'ApiController@insertCity');
+	Route::post('/submitproblem', 'ApiController@submitProblem')->middleware('check:2');
+	Route::get('/authenticate/user', 'AuthenticateController@getAuthenticatedUser');
+	Route::post('/authenticate', 'AuthenticateController@authenticate');
+	Route::post('/signup', 'AuthenticateController@signup');
+	Route::get('/users/{username?}/{email?}/{accesslevel?}/{banned?}', 'ApiController@getUsers');
+	Route::post('/ban/{id}/{time}', 'ApiController@banUser');
+	Route::get('/notification/{id}', 'ApiController@getNotifications');
+	Route::post('/notification/{id}', 'ApiController@hasSeenNotifications');
+	Route::get('/problems/{idcity}/{idcategory}', 'ApiController@getProblems');
+	Route::get('/problem/{id}', 'ApiController@getProblem');
+	Route::post('/submitcomment', 'ApiController@submitComment');
+	Route::post('/suggestCity', 'ApiController@suggestCity');
+	Route::post('/feedback', 'ApiController@feedback');
+	Route::post('/suggestCategory', 'ApiController@suggestCategory');
+	Route::get('/suggestCategory', 'ApiController@getSuggestCategories');
+	Route::post('/addCategory', 'ApiController@addCategory');
+	Route::post('/promoteUser', 'ApiController@promoteUser');
+	Route::post('/promoteUser', 'ApiController@suggestCR');
+});
+
+
+?>
